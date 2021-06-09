5afa5fc6b5d042cc8e2ab226586c32a73f1fd3de
Isaac Rabello <50160939+isaacyounes04@users.noreply.github.com>
2021-06-09 03:30:00 -0300

    Multi-user chat implementation

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
 import sys
+import os
+import threading
 import utils
 
 
 class Client:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = ''
+        self.messages = list()
+
         try:
             server = self.get_server()
             self.socket.connect(server)
@@ -62,7 +67,8 @@ class Client:
 
         if code == '02':
             print('[*] Successfully authenticated')
-            return True
+            self.name = username
+            self.messaging_loop()
 
         if code == '30':
             print('[!] Server refused to connect')
@@ -75,6 +81,40 @@ class Client:
 
         self.socket.close()
         sys.exit(1)
+
+
+    def messaging_loop(self):
+        t = threading.Thread(target=self.listen_for_messages)
+        t.daemon = True
+        t.start()
+
+        while True:
+            message = input('>>')
+            if message == '/quit':
+                print('[*] Closing connection and quiting...')
+                self.socket.close()
+                sys.exit(0)
+            else:
+                self.socket.send(f'[21]{message}'.encode())
+
+
+
+    def listen_for_messages(self):
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            for item in self.messages:
+                    print(item)
+
+            message = self.socket.recv(1024).decode()
+            code, text = utils.parse_message(message)
+
+            if code == '20' or code == '22':
+                self.messages.append(text)
+            else:
+                print(f'[!] Something went wrong... disconnecting from server.')
+                print('Type /quit to exit the program')
+                self.socket.close()
+                return
 
 
 
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import re
 import utils
+from datetime import datetime
 
 MESSAGE_MASK = re.compile('\[\d\d\].*')
 CREDENTIALS_MASK = re.compile('\[1\d\].{3,16}:.{3,16}')
@@ -12,7 +13,8 @@ class Server:
         self.ip = ip
         self.port = port
         self.name = name
-        self.clients = dict()
+        self.stored_users = dict()
+        self.active_clients = list()
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.ip, self.port))
@@ -38,7 +40,7 @@ class Server:
         if not CREDENTIALS_MASK.match(credentials):
             client_connection.send('[30]'.encode())
             client_connection.close()
-            return False
+            return
 
         code, text = utils.parse_message(credentials)
         user_name, password = text.split(':')
@@ -47,26 +49,60 @@ class Server:
             if self.handle_login(user_name, password):
                 print(f'[*] {user_name} logged in')
                 client_connection.send('[02]'.encode())
+                self.active_clients.append((client_connection, user_name))
+                self.messaging_loop(client_connection, user_name)
             else:
                 client_connection.send('[31]'.encode())
+                client_connection.close()
+                return
         elif code == '11':
             if self.handle_register(user_name, password):
-                client_connection.send('[02]'.encode())
                 print(f'[*] {user_name} registered')
+                client_connection.send('[02]'.encode())
+                self.active_clients.append((client_connection, user_name))
+                self.messaging_loop(client_connection, user_name)
             else:
                 client_connection.send('[32]'.encode())
+                client_connection.close()
+                return
 
 
     def handle_login(self, user_name, password):
-        return user_name in self.clients.keys() and password == self.clients[user_name]
+        return user_name in self.stored_users.keys() and password == self.stored_users[user_name]
 
 
     def handle_register(self, user_name, password):
-        if user_name in self.clients.keys():
+        if user_name in self.stored_users.keys():
             return False
         else:
-            self.clients[user_name] = password
+            self.stored_users[user_name] = password
             return True
+
+
+    def messaging_loop(self, client_connection, client_name):
+        client_connection.send(f'[20]Welcome to {self.name}!'.encode())
+        while True:
+            try:
+                message = client_connection.recv(1024).decode().strip()
+                if not MESSAGE_MASK.match(message):
+                    client_connection.send('[30]'.encode())
+                    client_connection.close()
+                    raise Exception
+
+                code, text = utils.parse_message(message)
+                if code == '21':
+                    text = f'[22]{datetime.now().hour}:{datetime.now().minute} <{client_name}> {text}'
+
+                    for client in self.active_clients:
+                        client[0].send(text.encode())
+
+            except:
+                text = f'[22]{datetime.now().hour}:{datetime.now().minute} <{client_name}> has left the chat'
+                for client in self.active_clients:
+                    client[0].send(text.encode())
+                    if client[0] is client_connection:
+                        self.active_clients.remove(client)
+                        client[0].close()
 
 
 server = Server('127.0.0.1', 7777, 'pyzza')
